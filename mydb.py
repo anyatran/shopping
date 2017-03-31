5b4a393f943853de0670cab397c7ba7f39dbc9b2
anyatran <panhtran249@gmail.com>
2017-03-30 21:37:31 -0400

    string format

--- mydb.py
+++ mydb.py
@@ -2,15 +2,13 @@ import json
 import sys
 import psycopg2
 
-con = con = psycopg2.connect("host='104.131.100.94' dbname='everlane' user='postgres' password='anyatran'")
+con = psycopg2.connect("host='104.131.100.94' dbname='everlane' user='postgres' password='anyatran'")
 con.autocommit = True
 cur = con.cursor()
 
 def init_db():
     try:
         view_products()
-        # view_users()
-        # view_history()
         while True:
             row = cur.fetchone()
             if row == None:
@@ -36,8 +34,8 @@ def view_products():
             break
         print ("ID: " + str(row[0]) + "\t\tProduct: " + row[2] + "\t\tPrice: " + str(row[1]))
 
-def view_cart():
-    cur.execute("SELECT * FROM cart")
+def view_cart(user_id):
+    cur.execute("SELECT * FROM cart WHERE user_id=" + user_id)
     print "==== cart: ===="
     while True:
         row = cur.fetchone()
@@ -46,24 +44,18 @@ def view_cart():
         print ("ID: " + str(row[0]) + "\t\tPID: " + str(row[1]) + "\t\tquant: " + str(row[2]))
 
 
-def view_history():
-    # cur.execute("CREATE TABLE History(Id SERIAL PRIMARY KEY, purchase JSON)")
-    # cur.execute("INSERT INTO History VALUES(DEFAULT, '{\"2\": 1}')")
-    # cur.execute("INSERT INTO History VALUES(default,'{\"1\": 4}')")
-    # cur.execute("INSERT INTO History VALUES(default, '{\"4\": 1}')")
-    # cur.execute("INSERT INTO History VALUES(default,'{\"1\": 2, \"2\": 1}')")
-    # cur.execute("INSERT INTO History VALUES(default,'{\"4\": 3}')")
-    cur.execute("SELECT * FROM History")
+def view_history(user_id):
+    cur.execute("SELECT * FROM History WHERE user_id=%s" % user_id)
     print "==== History: ===="
     while True:
         row = cur.fetchone()
         if row == None:
             break
-        print ("ID: " + str(row[0]) + "\t\tHistory: " + json.dumps(row[1]))
+        print ("Date: " + str(row[3]) + "\t\tProductID: " + str(row[1]) + "\t\tQuantity: " + str(row[2]))
 
 # what if items < cart?
-def add_to_cart(product_id, quantity):
-    product_cart = get_from_cart(product_id)
+def add_to_cart(user_id, product_id, quantity):
+    product_cart = get_from_cart(user_id, product_id)
     product = get_from_products(product_id)
 
     if product == None:
@@ -73,40 +65,56 @@ def add_to_cart(product_id, quantity):
 
     elif product_cart == None:
         if product[3] - quantity >= 0:
-            cur.execute("INSERT INTO cart VALUES(default," + product_id + "," +
-            str(quantity) + ")")
+            cur.execute("INSERT INTO Cart VALUES(default, %s, %d, %s)" % (product_id, quantity, user_id))
         else:
             print "cannot add because not enough inventory"
     else:
         new_quantity = product_cart[2] + quantity
         if product[3] - new_quantity >= 0:
-            cur.execute("UPDATE CART SET quantity=" + str(new_quantity) +
-            "WHERE product_id=" + product_id)
+            cur.execute("UPDATE CART SET quantity=%d WHERE id=%d" % (new_quantity, product_cart[0]))
         else:
             print "cannot add because not enough inventory"
-    view_cart()
+    view_cart(user_id)
 
-def remove_from_cart(product_id, quantity):
-    product_cart = get_from_cart(product_id)
+def remove_from_cart(user_id, product_id, quantity):
+    product_cart = get_from_cart(user_id, product_id)
     if product_cart != None:
         new_quantity = product_cart[2] - quantity
         if new_quantity < 0:
             print "can't remove more than currently in cart"
         elif new_quantity == 0:
-            cur.execute("DELETE FROM Cart WHERE product_id=" + product_id)
+            cur.execute("DELETE FROM Cart WHERE id=%d" % (product_cart[0]))
         else:
-            cur.execute("UPDATE CART SET quantity=" + str(new_quantity) +
-            "WHERE product_id=" + product_id)
+            cur.execute("UPDATE CART SET quantity=%d WHERE id=%d" % (new_quantity, product_cart[0]))
     else:
         print "cart doesnt contain that product"
 
+def purchase(user_id):
+    cur.execute("SELECT * FROM Cart WHERE user_id=%s" % (user_id))
+    cart = cur.fetchall()
+    print cart
+    if cart == None or len(cart) == 0:
+        print "the cart is empty"
+    else:
+        for cart_item in cart:
+            product = get_from_products(cart_item[1])
+            if product[3] - cart_item[2] >= 0:
+                cur.execute("INSERT INTO History VALUES(default, %d, %d, current_date, %s)" % (cart_item[1], cart_item[2], user_id))
+                cur.execute("DELETE FROM Cart WHERE id=%d" % (cart_item[0]))
+                decrement_product(cart_item[1], product[3] - cart_item[2])
+            else:
+                print "can't buy this item because not enough inventory"
+                continue
+
+def decrement_product(product_id, new_quantity):
+    cur.execute("UPDATE products SET available_inventory=%d WHERE id=%d" % (new_quantity, product_id))
 
 def get_from_products(product_id):
-    cur.execute("SELECT * FROM products WHERE id=" + product_id)
+    cur.execute("SELECT * FROM Products WHERE id=%s" % (product_id))
     row = cur.fetchone()
     return row
 
-def get_from_cart(product_id):
-    cur.execute("SELECT * FROM Cart WHERE product_id=" + product_id)
+def get_from_cart(user_id, product_id):
+    cur.execute("SELECT * FROM Cart WHERE product_id=%s and user_id=%s" % (product_id, user_id))
     row = cur.fetchone()
     return row
--- shop.py
+++ shop.py
@@ -3,28 +3,34 @@ import psycopg2
 import mydb
 
 class User:
-    def __init__(self):
-        self.user_id = None
+    def __init__(self, user_id):
+        self.id = user_id
         self.cart = None # {product_id: quantity}
         self.history_ids = [] # date: {items}
 
     def view_cart(self):
-        mydb.view_cart()
+        mydb.view_cart(self.id)
 
     # what if add the same item again?
     def add_to_cart(self, product_id, quantity):
         print "add %s to cart: %s" % (product_id, quantity)
-        mydb.add_to_cart(product_id, int(quantity))
+        mydb.add_to_cart(self.id, product_id, int(quantity))
 
     def remove_from_cart(self, product_id, quantity):
         print "remove %s from cart: %s" % (product_id, quantity)
-        mydb.remove_from_cart(product_id, int(quantity))
+        mydb.remove_from_cart(self.id, product_id, int(quantity))
 
     def purchase(self):
         print "purchase"
+        mydb.purchase(self.id)
 
-    def purchase_history(self):
-        print "purchase_history"
+    def view_history(self):
+        print "history"
+        mydb.view_history(self.id)
+
+    def switch_user(self, user_id):
+        print "switch user"
+        self.id = user_id
 
 def process(user):
     user_input = raw_input("command: ").split()
@@ -39,8 +45,10 @@ def process(user):
         user.view_cart()
     elif command == "purchase":
         user.purchase()
-    elif command == "purchase_history":
-        user.purchase_history()
+    elif command == "history":
+        user.view_history()
+    elif command == "user":
+        user.switch_user(args[0])
     elif command == "none" or command == "exit":
         print "DONE"
         return
@@ -50,6 +58,7 @@ def process(user):
 
 if __name__ == "__main__":
     mydb.init_db()
-    user = User()
+    user_input = raw_input("User ID: ").split()
+    user = User(user_input[0])
 
     process(user)
